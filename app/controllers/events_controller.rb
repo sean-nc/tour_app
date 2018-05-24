cf1c4cca7b0a7654677f7b7ba4e2b4a800ea0623
Sean <sean.n.cordes@gmail.com>
2018-05-24 12:03:51 -0400

    Made adjustments to views for events

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,6 +1,6 @@
 class EventsController < ApplicationController
   before_action :set_event, only: [:show, :edit, :update, :destroy]
-  before_action :logged_in?
+  before_action :logged_in?, only: [:new, :edit, :update, :destroy]
 
   def index
     @events = Event.all
---
+++ config/initializers/time_format.rb
@@ -0,0 +1,5 @@
+# Date
+Date::DATE_FORMATS[:default] = "%B %e, %Y"
+
+# Time
+Time::DATE_FORMATS[:default] = "%B %e, %Y - %l:%M %p"
