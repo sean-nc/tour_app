608c4f5927a804653bc34d72669227e0397bab84
Sean <sean.n.cordes@gmail.com>
2018-05-24 00:57:29 -0400

    added events controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  private
+    def logged_in?
+      unless user_signed_in?
+        flash[:danger] = "Please log in."
+        redirect_to new_user_session_path
+      end
+    end
 end
---
+++ app/controllers/events_controller.rb
@@ -0,0 +1,52 @@
+class EventsController < ApplicationController
+  before_action :set_event, only: [:show, :edit, :update, :destroy]
+  before_action :logged_in?
+
+  def index
+    @events = Event.all
+  end
+
+  def show
+  end
+
+  def new
+    @event = Event.new
+  end
+
+  def edit
+  end
+
+  def create
+  @event = Event.new(event_params)
+    if @event.save
+      redirect_to event_path(@event)
+    else
+      render 'new'
+    end
+  end
+
+  def update
+    if @event.update_attributes(event_params)
+      flash[:success] = "Event updated"
+      redirect_to @event
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @event.destroy
+    flash[:success] = "Event deleted"
+    redirect_to events_path
+  end
+
+  private
+    def set_event
+      @event = Event.find(params[:id])
+    end
+
+    def event_params
+      params.require(:event).permit(:title, :when, :duration ,:details,
+                                    :max_bookings, :price, :creator_id)
+    end
+end
