5107cec775a1566c540d2692fd29d44525527b3e
Sean <sean.n.cordes@gmail.com>
2018-05-24 23:06:08 -0400

    working stripe controller, needs upgrading

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   get     'about',      to: 'static_pages#about'
   get     'contact',    to: 'static_pages#contact'
   resources :events
+  resources :charges
   devise_for :users, controllers: { registrations: "registrations"}
 end
